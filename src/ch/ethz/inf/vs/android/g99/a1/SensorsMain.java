50587fffda41d355a3d91c07266a15df09ee0107
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-02 22:28:54 +0200

    Initial project import

---
+++ src/ch/ethz/inf/vs/android/g99/a1/SensorsMain.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.android.g99.a1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SensorsMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
