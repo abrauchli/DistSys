73355e4e4da9e53b77589a6c9adc0cca95a99d36
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-03 14:47:27 +0200

    Add item list handler

--- src/ch/ethz/inf/vs/android/g54/a1/SensorsMain.java
+++ src/ch/ethz/inf/vs/android/g54/a1/SensorsMain.java
@@ -3,25 +3,40 @@ package ch.ethz.inf.vs.android.g54.a1;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class SensorsMain extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         SensorManager m = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> sensors = m.getSensorList(Sensor.TYPE_ALL);
+        final List<Sensor> sensors = m.getSensorList(Sensor.TYPE_ALL);
         String[] sens = new String[sensors.size()];
         for (int i=0; i < sensors.size(); ++i) {
         	sens[i] = sensors.get(i).getName();
         }
         ListView lv = (ListView) findViewById(R.id.lv_sensorlist);
         lv.setAdapter(new ArrayAdapter<String>(this, R.layout.li_sensor, sens));
+        final SensorsMain me = this;
+        lv.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				sensors.get(arg2);
+				Intent sd = new Intent(me, SensorsDetail.class);
+				me.startActivity(sd);
+			}
+		});
     }
 }
