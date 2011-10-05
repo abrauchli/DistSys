8e6419d222fa98732b6fc9ab61cf2f1bc7110971
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-05 23:03:13 +0200

    Basic service implementation

--- A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftService.java
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftService.java
@@ -2,14 +2,44 @@ package ch.ethz.inf.vs.android.g54.a1;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 public class AntiTheftService extends Service {
 
+	// This is the object that receives interactions from clients. See
+	// RemoteService for a more complete example.
+	private final IBinder mBinder = new LocalBinder();
+
+	/**
+	 * Class for clients to access. Because we know this service always runs in
+	 * the same process as its clients, we don't need to deal with IPC.
+	 */
+	public class LocalBinder extends Binder {
+		AntiTheftService getService() {
+			return AntiTheftService.this;
+		}
+	}
+
 	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i("LocalService", "Received start id " + startId + ": " + intent);
+		// We want this service to continue running until it is explicitly
+		// stopped, so return sticky.
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		// Tell the user we stopped.
+		Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
 	}
 
 }
