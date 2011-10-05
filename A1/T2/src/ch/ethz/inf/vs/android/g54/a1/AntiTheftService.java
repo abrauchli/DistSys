d6c5e215f77d70bff7b5b13657c65482edf369ab
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-05 22:53:30 +0200

    Add and register (empty) service

---
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftService.java
@@ -0,0 +1,15 @@
+package ch.ethz.inf.vs.android.g54.a1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AntiTheftService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
