cf2cd0af0fd42a0b51cf282a3c66506e1f35d46e
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-05 23:45:27 +0200

    Initial gui logic

--- A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
@@ -2,6 +2,9 @@ package ch.ethz.inf.vs.android.g54.a1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
 public class AntiTheftMain extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,19 @@ public class AntiTheftMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void onDelayChanged(View v) {
+    	SeekBar b = (SeekBar) v;
+    	int delay = b.getProgress();
+    }
+    
+    public void onAlarmClick(View v) {
+    	ToggleButton b = (ToggleButton) v;
+    	if (b.isChecked()) {
+    		// enable service
+    	} else {
+    		// disable service
+    	}
     }
 }
