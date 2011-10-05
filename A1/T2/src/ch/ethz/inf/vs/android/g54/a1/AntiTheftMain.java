4a74e818bbdfe573e1eaaeae983eae0a8ecf0a41
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-05 22:47:39 +0200

    Rename MainActivity -> AntiTheftMain

--- A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
@@ -3,7 +3,7 @@ package ch.ethz.inf.vs.android.g54.a1;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class AntiTheftMain extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
