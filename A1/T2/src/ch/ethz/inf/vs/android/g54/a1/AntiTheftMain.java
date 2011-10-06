e3df782bc4f9fc04d54132504d367d0765fc86a5
Andreas Brauchli <a.brauchli@elementarea.net>
2011-10-06 16:03:48 +0200

    Basic start service handler, basic service logic

--- A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftMain.java
@@ -1,30 +1,31 @@
 package ch.ethz.inf.vs.android.g54.a1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.ToggleButton;
 
 public class AntiTheftMain extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    public void onDelayChanged(View v) {
-    	SeekBar b = (SeekBar) v;
-    	int delay = b.getProgress();
-    }
-    
-    public void onAlarmClick(View v) {
-    	ToggleButton b = (ToggleButton) v;
-    	if (b.isChecked()) {
-    		// enable service
-    	} else {
-    		// disable service
-    	}
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void onDelayChanged(View v) {
+		SeekBar b = (SeekBar) v;
+		int delay = b.getProgress();
+	}
+
+	public void onAlarmClick(View v) {
+		ToggleButton b = (ToggleButton) v;
+		if (b.isChecked()) {
+			startService(new Intent(this, AntiTheftService.class));
+		} else {
+			stopService(new Intent(this, AntiTheftService.class));
+		}
+	}
 }
--- A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftService.java
+++ A1/T2/src/ch/ethz/inf/vs/android/g54/a1/AntiTheftService.java
@@ -1,17 +1,26 @@
 package ch.ethz.inf.vs.android.g54.a1;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class AntiTheftService extends Service {
+public class AntiTheftService extends Service implements SensorEventListener {
 
 	// This is the object that receives interactions from clients. See
 	// RemoteService for a more complete example.
 	private final IBinder mBinder = new LocalBinder();
+
+	private NotificationManager mNM;
+	Sensor mSensor;
 
 	/**
 	 * Class for clients to access. Because we know this service always runs in
@@ -24,8 +33,14 @@ public class AntiTheftService extends Service {
 	}
 
 	@Override
+	public void onCreate() {
+		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+	}
+
+	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i("LocalService", "Received start id " + startId + ": " + intent);
+		initSensor();
 		// We want this service to continue running until it is explicitly
 		// stopped, so return sticky.
 		return START_STICKY;
@@ -34,12 +49,35 @@ public class AntiTheftService extends Service {
 	@Override
 	public void onDestroy() {
 		// Tell the user we stopped.
-		Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT)
+				.show();
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return mBinder;
+	}
+
+	private void initSensor() {
+		SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sm.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	private void ringAlarm() {
+		Notification notification = new Notification();
+		notification.tickerText = "THIEF ALARM";
+		notification.flags = Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR;
+		mNM.notify(0, notification);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor s, int acc) {
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent evt) {
+		// TODO
 	}
 
 }
