af3cfd863f58eb42f8efaa1b8b59457941927a10
m-egg <megg@student.ethz.ch>
2011-10-15 18:24:47 +0200

    First version of the GUI

---
+++ A2/T1/src/ch/ethz/inf/vs/android/g54/a2/MainActivity.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.android.g54.a2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	
+
+}
