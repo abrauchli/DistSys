19d46e8bbe78ba3064b2c88ec1ce24a75e50e0b5
m-egg <megg@student.ethz.ch>
2011-10-21 13:34:14 +0200

    Refactoring

--- A2/T1/src/ch/ethz/inf/vs/android/g54/a2/MainActivity.java
+++ A2/T1/src/ch/ethz/inf/vs/android/g54/a2/MainActivity.java
@@ -2,15 +2,12 @@ package ch.ethz.inf.vs.android.g54.a2;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
@@ -28,14 +25,13 @@ public class MainActivity extends Activity {
 	}
 	
 	public void onBtnHtmlRawClick (View v) {
-		Socket s;
 		try {
-			s = new Socket("vswot.inf.ethz.ch", 8081);			
+			Socket s = new Socket("vswot.inf.ethz.ch", 8081);			
 			
 			DataOutputStream outToServer = new DataOutputStream(s.getOutputStream());
 			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			
-			String message = "GET /sunspots/Spot1/sensors/temperature HTTP/1.1\nHost: vswot.inf.ethz.ch\n\n";
+			String message = "GET /sunspots/Spot1/sensors/temperature HTTP/1.1\r\nHost: vswot.inf.ethz.ch\r\n\r\n";
 			outToServer.writeBytes(message);
 			outToServer.flush();
 			
@@ -43,11 +39,11 @@ public class MainActivity extends Activity {
 			
 			String line = inFromServer.readLine();
 			
-			// TODO Takes too long, probably needs to be moved to a service.
-//			while (line != null) {
-//				sb.append(line + "\n");
-//				line = inFromServer.readLine();
-//			}
+			//Comment regarding performance
+			while (line != null) {
+				sb.append(line + "\n");
+				line = inFromServer.readLine();
+			}
 			outToServer.close();
 			inFromServer.close();
 			
@@ -56,29 +52,24 @@ public class MainActivity extends Activity {
 			t.setText(R.string.btn_html_raw);
 			t = (TextView) findViewById(R.id.txt_response);
 			
-			// TODO
-			t.setText(line);
-//			t.setText(sb.toString());
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			t.setText(sb.toString());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void onBtnHtmlApacheClick (View v) throws ClientProtocolException, IOException {		
+	public void onBtnHtmlApacheClick (View v) {		
 		HttpClient httpclient = new DefaultHttpClient();
 		String responseBody = "";
 		try {
-			HttpGet httpget = new HttpGet("http://vswot.inf.ethz.ch:8081/sunspots/Spot1/sensors/temperature");
+			HttpGet httpGet = new HttpGet("http://vswot.inf.ethz.ch:8081/sunspots/Spot1/sensors/temperature");
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			responseBody = httpclient.execute(httpget, responseHandler);
+			responseBody = httpclient.execute(httpGet, responseHandler);
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			httpclient.getConnectionManager().shutdown();
 		}
-		
 		
 		TextView t;
 		t = (TextView) findViewById(R.id.txt_type_of_request);
@@ -87,7 +78,7 @@ public class MainActivity extends Activity {
 		t.setText(responseBody);
 	}
 	
-	public void onBtnJsonRawClick (View v) throws ClientProtocolException, IOException {
+	public void onBtnJsonRawClick (View v) {
 		HttpClient httpclient = new DefaultHttpClient();
 		String responseBody = "";
 		try {
@@ -96,10 +87,11 @@ public class MainActivity extends Activity {
 			httpget.addHeader(header);
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
 			responseBody = httpclient.execute(httpget, responseHandler);
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			httpclient.getConnectionManager().shutdown();
 		}
-		
 		
 		TextView t;
 		t = (TextView) findViewById(R.id.txt_type_of_request);
@@ -108,19 +100,23 @@ public class MainActivity extends Activity {
 		t.setText(responseBody);
 	}
 	
-	public void onBtnJsonParsedClick (View v) throws ClientProtocolException, IOException {
+	public void onBtnJsonParsedClick (View v) {
 		HttpClient httpclient = new DefaultHttpClient();
 		String responseBody = "";
+		double temperature = 0;
+		
 		try {
 			HttpGet httpget = new HttpGet("http://vswot.inf.ethz.ch:8081/sunspots/Spot1/sensors/temperature");
 			BasicHeader header = new BasicHeader("Accept", "application/json");
 			httpget.addHeader(header);
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
 			responseBody = httpclient.execute(httpget, responseHandler);
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			httpclient.getConnectionManager().shutdown();
 		}
-		double temperature = 0;
+		
 		try {
 			JSONObject jsonObject = new JSONObject(responseBody);
 			temperature = jsonObject.getDouble("value");
